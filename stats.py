157ab7dd1f07a3c7c2e0610d99c9ea4d164838fe
xiaojay <xiaojay@gmail.com>
2020-01-05 09:17:43 +0000

    update stats

--- stats.py
+++ stats.py
@@ -2,16 +2,19 @@ import redis, datetime
 stream = 'shares2'
 client = redis.Redis(host='localhost', port=6379, db=0, charset="utf-8", decode_responses=True)
 
-def get_minute_range(dt):
+def get_minute_range(dt, mins=1):
     dt_start = datetime.datetime(dt.year, dt.month, dt.day, dt.hour, dt.minute)
     dt_end = dt_start + datetime.timedelta(minutes=1)
+    dt_start = dt_start - datetime.timedelta(minutes=mins)
     start = int(dt_start.timestamp() * 1000)
     end = int(dt_end.timestamp() * 1000 - 1)
     return [start, end]
 
 now = datetime.datetime.now() - datetime.timedelta(minutes=1)
 
-start, end = get_minute_range(now)
+start, end = get_minute_range(now, 1)
+print(start)
+print(end)
 data = client.xrange(stream, start, end)
 #print(data)
 
